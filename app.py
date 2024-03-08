d42f3ed772e0c7cbae8764df9e3ea8fdee651ef9
natakulik <natakulik25@gmail.com>
2024-03-08 20:53:00 +0200

    Simple calculator with add and substract functions

---
+++ app.py
@@ -0,0 +1,28 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("fun")
+parser.add_argument("num1")
+parser.add_argument("num2")
+
+args = parser.parse_args()
+
+#print(args.num1, args.num2)
+
+
+def add(num1, num2):
+    return num1 + num2
+
+
+def sub(num1, num2):
+    return num1 - num2
+
+
+res = 0
+if args.fun == 'add':
+    res = add(int(args.num1), int(args.num2))
+else:
+    res = sub(int(args.num1), int(args.num2))
+
+print('Result of the operation:', args.fun, res, sep=' ')
+print('done')
